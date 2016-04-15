2eb4c22484b153d85cb3f51d8eefc3a0310a4698
Divya Patel <divyakumar.patel@sjsu.edu>
2016-04-14 23:16:35 -0700

    impact logic between leaser and rock  closes#9

--- Bullet.java
+++ Bullet.java
@@ -12,13 +12,38 @@ public class Bullet extends Actor
    public void act() 
    {
         move(45); 
-        
+          eatRock();
             
-        if (getX() > 1280)
-        {
-            rocket.weapon=null;
-            getWorld().removeObject(this);
-        }
+        
    } 
-  
+ 
+   
+    private void eatRock()
+    {
+        Actor Rock = getOneObjectAtOffset(0,0, Rock.class);             
+        if(Rock !=null) 
+        {
+             
+            World myWorld = getWorld();
+            MyWorld crabspace = (MyWorld)myWorld;
+            Counter counter = crabspace.getCounter();
+            counter.addScore();
+            rocket.weapon=null;
+             getWorld().removeObject(Rock); 
+        }
+        else
+        {
+            if (getX() > 1280)
+            {
+               rocket.weapon=null;
+                //  new Rock(this);
+                getWorld().removeObject(this);
+               // schuss.setRotation(0);
+               // getCrabWorld().addObject(1290, 450);
+                
+            }
+        }
+    }
+   
+   
 }
