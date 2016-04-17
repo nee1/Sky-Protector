5fdbecce4776c5287e9afb8b26f529da280cdc0d
Anuvrat Tiku <avtrulzz@yahoo.co.in>
2016-04-16 17:29:21 -0700

    Added another spawner #23
    added fly and lady bug spawners

---
+++ Fly.java
@@ -0,0 +1,99 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fly here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fly extends Spawner
+{
+    
+    int shotstaken=0;
+    int ballstaken=0;
+    int enemykilled=0;
+    
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        GreenfootImage img = getImage();
+        img.scale(20,20);
+        setImage(img);
+        moveAround();
+        
+        Actor Bullet=getOneIntersectingObject(Bullet.class); 
+        if (Bullet!=null)
+        {
+            getWorld().removeObject(Bullet);
+            shotstaken++;
+        }
+        
+        Actor Shot=getOneIntersectingObject(Shot.class);
+        if (Shot!=null)
+        {
+            getWorld().removeObject(Shot);
+            shotstaken++;
+        }
+        
+        Actor Ball=getOneIntersectingObject(Ball.class);
+        if (Ball!=null)
+        {
+            getWorld().removeObject(Ball);
+            shotstaken++;
+        }
+       
+        if (shotstaken>=1)
+        {
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(30);
+            Killed counter2 = spaceWorld.getKilled();
+            counter2.bumpCount(1);
+            enemykilled++;
+            getWorld().removeObject(this);
+        }
+        
+        if (ballstaken>=1)
+        {
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-30);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void moveAround()
+    {
+        if (enemykilled<50)
+        {
+            move(6);
+        }
+        
+        if (enemykilled>=50)
+        {
+            move(12);
+        }
+        
+        if (Greenfoot.getRandomNumber(100)<10)
+        {
+            turn(Greenfoot.getRandomNumber(180));
+        }
+        
+        if (getX() <= 5 || getX() >= getWorld().getWidth() - 5)
+        {
+            turn(180);
+        }
+        
+        if (getY() <= 5 || getY() >= getWorld().getHeight() - 5)
+        {
+            turn(180);
+        }      
+    }
+}
---
+++ Lady.java
@@ -0,0 +1,189 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Special here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lady extends Spawner
+{
+    int shotstaken=0;
+    int ladykilled=0;
+    int ballstaken=0;
+    
+    public void act() 
+    
+    {
+        moveAround();
+        eat();
+        
+        GreenfootImage img = getImage();
+        img.scale(100,100);
+        setImage(img);
+        
+        Actor Bullet=getOneIntersectingObject(Bullet.class);
+        if (Bullet!=null)
+        {
+            getWorld().removeObject(Bullet);
+            shotstaken++;
+        }
+        
+        Actor Shot=getOneIntersectingObject(Shot.class);
+        if (Shot!=null)
+        {
+            getWorld().removeObject(Shot);
+            shotstaken++;
+        }
+        
+        Actor Ball=getOneIntersectingObject(Ball.class);
+        if (Ball!=null)
+        {
+            getWorld().removeObject(Ball);
+            ballstaken++;
+        }
+        
+        if (shotstaken>=30)
+        {
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(500);
+            Killed counter2 = spaceWorld.getKilled();
+            counter2.bumpCount(1);
+            ladykilled++;
+            getWorld().removeObject(this);
+        }
+        
+        if (ballstaken>=30)
+        {
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-500);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void moveAround()
+    {
+        if (ladykilled<5)
+        {
+            move(2);
+        }
+        
+        if (ladykilled>=5)
+        {
+            move(4);
+        }
+        
+        if (Greenfoot.getRandomNumber(100)<3)
+        {
+            turn(Greenfoot.getRandomNumber(180));
+        }
+        
+        if (getX() <= 5 || getX() >= getWorld().getWidth() - 5)
+        {
+            turn(180);
+        }
+        
+        if (getY() <= 5 || getY() >= getWorld().getHeight() - 5)
+        {
+            turn(180);
+        }      
+    }
+    
+    public void eat()
+    {
+        Actor Enemy;
+        Enemy = getOneObjectAtOffset(0,0,Enemy.class);
+        if (Enemy !=null)
+        {
+            World Space;
+            Space = getWorld();
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-10);
+            Space.removeObject(Enemy);
+        }
+        
+        Actor Bee;
+        Bee = getOneObjectAtOffset(0,0,Bee.class);
+        if (Bee !=null)
+        {
+            World Space;
+            Space = getWorld();
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();  
+            counter.bumpCount(-20);
+            Space.removeObject(Bee);
+        }
+        
+        Actor Spider;
+        Spider = getOneObjectAtOffset(0,0,Spider.class);
+        if (Spider !=null)
+        {
+            World Space;
+            Space = getWorld();
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-30);
+            Space.removeObject(Spider);
+        }
+        
+        Actor Rock;
+        Rock = getOneObjectAtOffset(0,0,Rock.class);
+        if (Rock !=null)
+        {
+            World Space;
+            Space = getWorld();
+            Space.removeObject(Rock);
+        }
+        
+        Actor Rock2;
+        Rock2 = getOneObjectAtOffset(0,0,Rock2.class);
+        if (Rock2 !=null)
+        {
+            World Space;
+            Space = getWorld();
+            Space.removeObject(Rock2);
+        }
+        
+        Actor Lady;
+        Lady = getOneObjectAtOffset(0,0,Lady.class);
+        if (Lady !=null)
+        {
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-500);
+            World Space;
+            Space = getWorld();
+            Space.removeObject(Lady);
+        }
+        
+        Actor Plane;
+        Plane = getOneObjectAtOffset(0,0,Plane.class);
+        if (Plane !=null)
+        {
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-1000);
+            World Space;
+            Space = getWorld();
+            Space.removeObject(Plane);
+        }
+        
+        Actor Baby;
+        Baby = getOneObjectAtOffset(0,0,Baby.class);
+        if (Baby !=null)
+        {
+            Space spaceWorld = (Space) getWorld();
+            Score counter = spaceWorld.getScore();
+            counter.bumpCount(-1000);
+            World Space;
+            Space = getWorld();
+            Space.removeObject(Baby);
+        }
+    }
+    }   
