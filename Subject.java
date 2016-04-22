b923bd4aaf23c6adb832124e9d9e075d7d9f3409
Shalin Amin <shalinamin84@outlook.com>
2016-04-21 23:58:34 -0700

    add observer and subject for kills and score #30

---
+++ Subject.java
@@ -0,0 +1,11 @@
+/**
+ * Write a description of class Subject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Subject  
+{
+    public void attach(Observer o);
+    public void notifyO();
+}
