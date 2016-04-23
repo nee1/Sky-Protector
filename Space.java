23006fd6dbdb30134040f6d6a21c3f3e773faba1
Anuvrat Tiku <avtrulzz@yahoo.co.in>
2016-04-23 16:32:45 -0700

    Generated enemy using Factory method closes issue #29
    Generate enemy using factory closes issue #29

--- Space.java
+++ Space.java
@@ -94,49 +94,84 @@ public class Space extends World
     
     public void act()
     {
-        timer++;
+       timer++;
         planespawn++;
+        int selectedEnemy = 0;
+        EnemyFactory enemy = new EnemyFactory();
+        EnemyClass enemyClass = null;
         
-        if (timer>=100)
-        {if (Greenfoot.getRandomNumber(100)<5)
-        {addObject(new Enemy(), (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));}
+        if(timer>=100)
+        {
+             selectedEnemy = Greenfoot.getRandomNumber(20);
+            if (selectedEnemy==0)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
         }
         
-        if (timer>=2500)
-        {if (Greenfoot.getRandomNumber(1500)==1)
-        {addObject(new Lady(), 500, 300);}
+        if(timer>=500)
+        {
+            selectedEnemy = Greenfoot.getRandomNumber(80);
+            if (selectedEnemy==1)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
         }
         
-        if (timer>=1000)
-        {if (Greenfoot.getRandomNumber(700)<5)
-        {addObject(new Bee(), (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));}
+        if(timer>=500)
+        {
+            selectedEnemy = Greenfoot.getRandomNumber(80);
+            if (selectedEnemy==2)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
         }
         
-        if (timer>=2000)
-        {if (Greenfoot.getRandomNumber(1500)<5)
-        {addObject(new Spider(), (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));}
+         if(timer>=1000)
+        {
+            selectedEnemy = Greenfoot.getRandomNumber(140);
+            if (selectedEnemy==3)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
         }
         
-        if (timer>=500)
-        {if (Greenfoot.getRandomNumber(4000)<15)
-        {addObject(new Rock(), 3, (Greenfoot.getRandomNumber(600)));}
+        if(timer>=1500)
+        {
+            selectedEnemy = Greenfoot.getRandomNumber(240);
+            if (selectedEnemy==4)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
         }
         
-        if (timer>=500)
-        {if (Greenfoot.getRandomNumber(4000)<15)
-        {addObject(new Rock2(), 997, (Greenfoot.getRandomNumber(600)));}
+        if(timer>=2000)
+        {
+            selectedEnemy = Greenfoot.getRandomNumber(300);
+             if (selectedEnemy==5)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
+        }
+                
+        
+        if(timer>=2500)
+        {
+            selectedEnemy = Greenfoot.getRandomNumber(1500);
+            if (selectedEnemy==6)
+            {
+                enemyClass = enemy.makeEnemyFactory(selectedEnemy);
+                addObject(enemyClass, (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));
+            }
         }
         
-        if (timer>=1500)
-        {if (Greenfoot.getRandomNumber(1200)<5)
-        {addObject(new Fly(), (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));}
-        }
-        
-        if (timer>=100)
-        {if (Greenfoot.getRandomNumber(2500)==1)
-        {addObject(new Baby(), (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));}
-        }
-        
+ 
         if (timer>=100)
         {if (Greenfoot.getRandomNumber(5000)==1)
         {addObject(new Life(), (Greenfoot.getRandomNumber(1000)), (Greenfoot.getRandomNumber(600)));}
@@ -152,5 +187,6 @@ public class Space extends World
             addObject(thePHealth, 970, 30);
             planespawn=0;
         }
+    
     }
 }
