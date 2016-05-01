1636974e80e337ca10c65a4d9763311055ca0a67
Shalin Amin <shalinamin84@outlook.com>
2016-04-30 17:27:53 -0700

    implementing observer for bee #30

--- Bee.java
+++ Bee.java
@@ -6,14 +6,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Bee extends EnemyClass
+public class Bee extends EnemyClass implements Subject
 {
     int shotstaken=0;
     int ballstaken=0;
     int count=0;
     int health=100;
     int beekilled=0;
-    
+    ArrayList<Observer> observers;
+     public Enemy(){
+        observers = new ArrayList<Observer>();        
+    }
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -53,9 +56,13 @@ public class Bee extends EnemyClass
             //getWorld().addObject(new Enemy(),(Greenfoot.getRandomNumber(1000)),(Greenfoot.getRandomNumber(600)));
             Space spaceWorld = (Space) getWorld();
             Score counter = spaceWorld.getScore();
-            counter.bumpCount(20);
             Killed counter2 = spaceWorld.getKilled();
-            counter2.bumpCount(1);
+            attach(counter);
+            attach(counter2);
+            notifyO();
+            // counter.bumpCount(20);
+            // Killed counter2 = spaceWorld.getKilled();
+            // counter2.bumpCount(1);
             beekilled++;
             getWorld().removeObject(this);
         }
@@ -97,5 +104,18 @@ public class Bee extends EnemyClass
         {
             turn(180);
         }      
+    }
+    public void attach(Observer o){
+        observers.add(o);
+    }
+    public void notifyO(){
+        for(Observer o : observers){
+            if(o instanceof Score){
+            o.ItotalCount(20);
+            }
+            else if(o instanceof Killed){
+            o.ItotalCount(1);
+            }
+        }
     }
 }
